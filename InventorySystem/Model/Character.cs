475493d304ea63551c09a7bcf5af008de691a478
myglarn <alex.mattila@gmail.com>
2015-01-21 14:05:44 +0100

    Startat projektet

---
+++ InventorySystem/Model/Character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Model
+{
+    class Character
+    {
+        string m_name;
+        int m_exp;
+
+    }
+}
---
+++ InventorySystem/Model/Level.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Model
+{
+    class Level
+    {
+        int m_level;
+        int m_nextLevel;
+    }
+}
